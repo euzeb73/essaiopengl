49a6b7b83f12854bc375da7e51e8a4b5309d60dc
euzeb73 <euzeb73@github.com>
2023-11-01 22:06:20 +0100

    Initialisation

---
+++ essai.py
@@ -0,0 +1,55 @@
+import pygame as pg
+import moderngl as mgl
+import sys
+
+class GraphicsEngine:
+    def __init__(self, win_size=(800, 450)):
+        # init pygame modules
+        pg.init()
+        # window size
+        self.WIN_SIZE = win_size
+        # set opengl attr
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        # create opengl context
+        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        # mouse settings
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+        # detect and use existing opengl context
+        self.ctx = mgl.create_context()
+        self.clock = pg.time.Clock()
+        self.time = 0
+        self.delta_time = 0
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                # self.mesh.destroy()
+                pg.quit()
+                sys.exit()
+
+    def render(self):
+        # clear framebuffer
+        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        # render scene
+        # self.scene.render()
+        # swap buffers
+        pg.display.flip()
+
+    def get_time(self):
+        self.time = pg.time.get_ticks() * 0.001
+
+    def run(self):
+        while True:
+            self.get_time()
+            self.check_events()
+            # self.camera.update()
+            self.render()
+            self.delta_time = self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    app = GraphicsEngine()
+    app.run()
